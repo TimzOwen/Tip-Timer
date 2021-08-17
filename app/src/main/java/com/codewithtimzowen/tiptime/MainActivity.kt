d57e22188b4d5d41a61fbad587399d056628126a
Timz Owen <ktimothy@kabarak.ac.ke>
2021-08-17 18:36:19 +0300

    updated the calculate tip function

--- app/src/main/java/com/codewithtimzowen/tiptime/MainActivity.kt
+++ app/src/main/java/com/codewithtimzowen/tiptime/MainActivity.kt
@@ -3,6 +3,7 @@ package com.codewithtimzowen.tiptime
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.codewithtimzowen.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,6 +15,31 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //add click listener to the button
+        binding.calculateButton.setOnClickListener { calculateTip() }
+
+    }
+
+
+    // function to calculate the tip
+    private fun calculateTip() {
+        val stringEditText = binding.costOfService.text.toString()
+        val cost = stringEditText.toDouble()
+        val roudUp = binding.roundUpSwitch.isChecked
+
+        // get the checked radio button
+        val checkedId = binding.tipOption.checkedRadioButtonId
+        val tipPercentage = when(checkedId){
+            R.id.twenty_option -> 0.20
+            R.id.eighteen_option -> 0.18
+            else -> 0.15
+        }
+        var tip = tipPercentage * cost
+        if(roudUp){
+            tip = kotlin.math.ceil(tip)
+        }
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount,formattedTip)
 
     }
 
