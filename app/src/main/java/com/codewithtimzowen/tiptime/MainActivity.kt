10614cc51cf136d834b52cbe6a9736881aa610fe
Timz Owen <ktimothy@kabarak.ac.ke>
2021-08-17 18:09:17 +0300

    updated to viewBinding

--- app/src/main/java/com/codewithtimzowen/tiptime/MainActivity.kt
+++ app/src/main/java/com/codewithtimzowen/tiptime/MainActivity.kt
@@ -2,10 +2,19 @@ package com.codewithtimzowen.tiptime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.codewithtimzowen.tiptime.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // initialize binding here
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
+
 }
